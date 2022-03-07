72c86fb327108f0d2344b04efe7f51e2cb659988
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-03-07 19:30:31 +0000

    Exemplo

---
+++ Tableless.java
@@ -0,0 +1,12 @@
+public class Tableless{
+
+    public static void main (String[] args){
+
+        int x = 15;
+
+        while (x < 18){
+            System.out.println("Você não tem permissão");
+            x++;
+        }
+    }
+}
